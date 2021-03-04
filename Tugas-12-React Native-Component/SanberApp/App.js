f7dd2c2adf208d3edb50a2c7b9e6c5a2e3d2baa5
DeniJo <denijosuasamosir@gmail.com>
2021-03-04 22:53:19 +0700

    Menambahkan Tugas 14 RestApi

--- Tugas-12-React Native-Component/SanberApp/App.js
+++ Tugas-12-React Native-Component/SanberApp/App.js
@@ -2,29 +2,21 @@ import React from 'react'
 import { Platform, StyleSheet, Text, View, StatusBar } from 'react-native'
 import LoginForm from './Tugas-13/LoginForm.js'
 import AboutPage from './Tugas-13/AboutPage.js'
+import RestApi from './Tugas-14/RestApi'
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor='#67C5E5'
-      barStyle='light-content'/>
-      {/* <Text style={{
-        color:'#fff', 
-        fontSize:18}}>
-        Welcome to my page</Text> */}
-        <LoginForm/>
-    </View>
+
+export default function App(){
+  return(
+    <RestApi/>
   )
 }
-
-export default App
 
 const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:'#67C5E5',
-    // alignItems:'center',
-    // justifyContent:'center'
+    alignItems:'center',
+    justifyContent:'center'
     
   }
 })
